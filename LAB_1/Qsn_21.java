1659ae052e4980e495247bedbeb23296eb791ddf
20al <bishallimbu00010@gmail.com>
2025-06-11 09:47:53 +0545

    Qsn_21-Lab: finished

---
+++ LAB_1/Qsn_21.java
@@ -0,0 +1,8 @@
+public class Qsn_21 {
+    public static void main(String[] args) {
+        System.out.println("Natural numbers upto 100:");
+        for(int i = 1; i <= 100; i++) {
+            System.out.printf("%4d",i);
+        }
+    }
+}
