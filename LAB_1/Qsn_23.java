33ea012d0cb24a610dc89d6cfc4288c38cbd2b52
20al <bishallimbu00010@gmail.com>
2025-06-11 10:04:20 +0545

    Qsn_23-Lab: finished

---
+++ LAB_1/Qsn_23.java
@@ -0,0 +1,12 @@
+public class Qsn_23 {
+    public static void main(String[] args) {
+        int sum = 0;
+        for(int i = 1; i <= 50; i++) {
+            sum += i;
+        }
+        System.out.println("Sum of all natural numbers upto 50 = "+sum);
+        sum = 50*51/2; // using formula sum of n natural numbers = n*(n+1)/2
+        System.out.println("Sum of all natural numbers upto 50 = "+sum);
+
+    }
+}
