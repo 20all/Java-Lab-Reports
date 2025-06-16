fbc1479cac4deff1cf03937b8ecc17fd2f5da4ff
20al <bishallimbu00010@gmail.com>
2025-06-16 15:15:28 +0545

    Qsn_7E-Lab3

---
+++ LAB_3/Qsn_7E.java
@@ -0,0 +1,22 @@
+public class Qsn_7E {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+    // public static void main(String[] args) {
+    //     for(int i = 1; i <= 5; i++) {
+    //         for(int j = 5; j >= 1; j--) {
+    //             if(j>i) {
+    //                 System.out.printf(" ");
+    //             }else {
+    //             System.out.printf("%d",i);
+    //             }
+    //         }
+    //         System.out.print("\n");
+    //     }
+    // }
