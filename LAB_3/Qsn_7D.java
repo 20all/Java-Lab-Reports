11387773e9b470e1918fc326449028bd412729de
20al <bishallimbu00010@gmail.com>
2025-06-16 09:48:00 +0545

    Qsn_7D-Lab3

---
+++ LAB_3/Qsn_7D.java
@@ -0,0 +1,10 @@
+public class Qsn_7D {
+    public static void main(String[] args) {
+        for(int i = 5; i >= 1; i--) {
+            for(int j = 1; j <= i; j++) {
+                System.out.printf("%d",i);
+            }
+            System.out.print("\n");
+        }
+    }
+}
