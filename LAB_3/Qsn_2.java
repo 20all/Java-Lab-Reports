1bbb5db7f27d43e5b87e895e394191f8084f423e
20al <bishallimbu00010@gmail.com>
2025-06-16 08:30:06 +0545

    Qsn_2-Lab3-unfinished

---
+++ LAB_3/Qsn_2.java
@@ -0,0 +1,18 @@
+public class Qsn_2 {
+    public static void main(String[] args) {
+        int n = 10;
+        int acc = 0;
+        for(int i = 2; i <= n; i++) {
+            for(int j = 2; j < i; j++) {
+                if(i%j == 0) {
+                    acc++;
+                    break;
+                }
+            }
+            System.out.printf(" %d",acc);
+            // if(acc!=0) {
+            //     System.out.printf("%4d",i);
+            // } 
+        }
+    }
+}
