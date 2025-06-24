a3e7aeebd205f47d615afad94b8b5e2e62c82ac0
20al <bishallimbu00010@gmail.com>
2025-06-24 16:25:37 +0545

    Qsn_7 and Qsn_8 -Lab4

---
+++ LAB_4/Qsn_8.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Qsn_8 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int size = 10;
+        System.out.println("Enter any "+size+" numbers:");
+        int[] n = new int[size];
+        for(int i = 0; i < size; i++) {
+            n[i] = input.nextInt();
+        } 
+        int temp;
+        for(int i = 0; i < size; i++) {
+            for(int j = i+1; j < size; j++) {
+                if(n[i] < n[j]) {
+                    temp = n[i];
+                    n[i] = n[j];
+                    n[j] = temp;
+                }
+            }
+        }
+        System.out.println("Numbers in descending order: ");
+        System.out.println(Arrays.toString(n));
+        input.close();
+    }
+}
