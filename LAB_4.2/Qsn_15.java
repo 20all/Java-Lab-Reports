029bb5cb8c11824f225e010b9b70d0232e76552c
20al <bishallimbu00010@gmail.com>
2025-06-27 10:07:15 +0545

    Qsn_15-Lab4.2

---
+++ LAB_4.2/Qsn_15.java
@@ -0,0 +1,48 @@
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+public class Qsn_15 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int row,col;
+        int size = 2;
+        row = col = size;
+        int[][] matrix1 = new int[row][col];
+        int[][] matrix2 = new int[row][col];
+        System.out.println("Enter the elements for matrix 1:");
+        for(int i = 0; i < row; i++) {
+            for(int j = 0; j < col; j++) {
+                matrix1[i][j] = input.nextInt();
+            }
+        }
+        System.out.println("Enter the elements for matrix 2:");
+        for(int i = 0; i < row; i++) {
+            for(int j = 0; j < col; j++) {
+                matrix2[i][j] = input.nextInt();
+            }
+        }
+        int sum = 0;
+        int[][] sumMatrix = new int[row][col]; // initializing sumMatrix
+        // adding two matrices and storing it in sumMatrix
+        for(int i = 0; i < row; i++) {
+            for(int j = 0; j < col; j++) {
+                sum += matrix1[i][j] + matrix2[i][j];
+                sumMatrix[i][j] = sum;
+            }
+        }
+        //displaying sumMatrix
+        System.out.println("Sum of the matrices:");
+        for(int[] e : sumMatrix) {
+            System.out.println(Arrays.toString(e));
+        }
+        
+        // next approach: without iteration
+        int[][] matrixSum = IntStream.range(0, size).mapToObj(i -> IntStream.range(0, size).map(j -> matrix1[i][j] + matrix2[i][j]).toArray()).toArray(int[][]::new); 
+        System.out.println("Sum of the matrices:");
+        for(int[] i : matrixSum) {
+            System.out.println(Arrays.toString(matrixSum));
+        }
+        input.close();
+    }
+}
