75422679a5e9469ec5494526422951dfdc6aa2d2
20al <bishallimbu00010@gmail.com>
2025-06-28 16:21:57 +0545

    Deleted the comments which where the previous code

--- LAB_5/Qsn_18.java
+++ LAB_5/Qsn_18.java
@@ -35,27 +35,9 @@ class Prime {
     public List<Integer> getPrimeNumbers() {
         return primeNumbers;
     }
-    // void primeGenerator() {
-    //     for(int i = 2; i <= n; i++) {
-    //         int acc = 0;
-    //         for(int j = 2; j < i; j++) {
-    //             if(i % j == 0) {
-    //                 acc++;
-    //                 break;
-    //             }
-    //         }
-    //         if(acc == 0) {
-    //             System.out.print(i+" ");
-    //         }
-    //     }
-    // }
 }
 public class Qsn_18 {
     public static void main(String[] args) {
-        // int n = 100;
-        // Prime prime = new Prime(n);
-        // System.out.printf("Prime numbers from %d to %d%n:",0,n);
-        // prime.primeGenerator();
         Prime prime = new Prime(1, 100); // creating Prime object from 1 to 100
         prime.generatePrime(); // generating prime numbers and adding them to the list
         System.out.println(prime.getPrimeNumbers()); // get the prime numbers added in the list
