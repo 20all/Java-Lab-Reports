0eb1fa22bea6a6587be40ecee032ce51e761651e
20al <bishallimbu00010@gmail.com>
2025-06-28 15:52:40 +0545

    Qsn_18-Lab5 modified

--- LAB_5/Qsn_18.java
+++ LAB_5/Qsn_18.java
@@ -1,14 +1,9 @@
-import java.util.Arrays;
-import java.util.stream.IntStream;
-
 class Prime {
     private int n;
     Prime(int number) {
         this.n = number;
     }
     void primeGenerator() {
-        // int[] numbers = IntStream.range(2, n).toArray();
-        // int[] primeNumbers;
         for(int i = 2; i <= n; i++) {
             int acc = 0;
             for(int j = 2; j < i; j++) {
@@ -17,12 +12,10 @@ class Prime {
                     break;
                 }
             }
-            // System.out.print(acc+" ");
             if(acc == 0) {
                 System.out.print(i+" ");
             }
         }
-        // return primeNumbers;
     }
 }
 public class Qsn_18 {
@@ -31,7 +24,6 @@ public class Qsn_18 {
         Prime prime = new Prime(n);
         System.out.printf("Prime numbers from %d to %d%n:",0,n);
         prime.primeGenerator();
-        // System.out.println(Arrays.toString(primeNumbers));
     }
 }
 
