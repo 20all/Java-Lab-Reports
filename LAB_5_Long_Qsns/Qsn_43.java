7fd9eaf4ef7d974a395733ff165cc4365714f824
20al <bishallimbu00010@gmail.com>
2025-07-06 13:41:14 +0545

    finished Lab5

--- LAB_5_Long_Qsns/Qsn_43.java
+++ LAB_5_Long_Qsns/Qsn_43.java
@@ -8,6 +8,9 @@ class Animal {
     public void makeSound() {
         System.out.println("Animals make sound.....");
     }
+    public int getAge() {
+        return age;
+    }
 }
 
 class Lion extends Animal {
@@ -51,16 +54,24 @@ class Zebra extends Animal {
 }
 public class Qsn_43 {
     public static void main(String[] args) {
-        Animal animal = new Animal("animal", 0);
+        Animal animal = new Animal("animal", 1);
         Animal lion = new Lion("lion", 5);
         Animal tiger = new Tiger("tiger", 7);
         Animal giraffe = new Giraffe("giraffe", 2);
         Animal zebra = new Zebra("zebra", 5);
 
         animal.makeSound();
+        System.out.println("Age : "+animal.getAge());
         lion.makeSound();
         tiger.makeSound();
         giraffe.makeSound();
         zebra.makeSound();
     }
 }
+// -> OUTPUT
+// Animals make sound.....
+// Age : 1
+// Lions make sound.
+// Tigers make sound.
+// Giraffe make sound.
+// Zebras make sound.
