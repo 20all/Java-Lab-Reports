cc09500a05d698bbd6ab4dd28c456a89bf19a6c1
20al <bishallimbu00010@gmail.com>
2025-07-06 14:01:26 +0545

    modified the wrond code of Qsn_2

--- LAB_3/Qsn_2.java
+++ LAB_3/Qsn_2.java
@@ -1,18 +1,21 @@
 public class Qsn_2 {
-    public static void main(String[] args) {
-        int n = 10;
-        int acc = 0;
-        for(int i = 2; i <= n; i++) {
-            for(int j = 2; j < i; j++) {
-                if(i%j == 0) {
-                    acc++;
-                    break;
-                }
+    public static boolean isPrime(int n) {
+        if(n <= 1) {
+            return false;
+        }
+        for(int i = 2; i*i <= n; i++){
+            if(n % i == 0) {
+                return false;
             }
-            // System.out.printf(" %d",acc);
-            if(acc!=0) {
-                System.out.printf("%4d",i);
-            } 
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        int n = 100;
+        for(int i = 2; i <= n; i++) {
+            if(isPrime(i)) {
+                    System.out.print(i+" ");
+                } 
         }
     }
 }
